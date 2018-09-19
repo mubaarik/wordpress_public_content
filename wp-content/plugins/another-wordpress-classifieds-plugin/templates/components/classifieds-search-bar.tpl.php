7dc5a69f9701348700f9259fef63caeddf10d7b4
inasarhaye <mmdmubaarik@gmail.com>
2018-09-19 16:38:57 +0300

    removing the find listings

--- wp-content/plugins/another-wordpress-classifieds-plugin/templates/components/classifieds-search-bar.tpl.php
+++ wp-content/plugins/another-wordpress-classifieds-plugin/templates/components/classifieds-search-bar.tpl.php
@@ -5,8 +5,7 @@
             <input type="text" name="keywordphrase" />
         </div>
         <div class="awpcp-classifieds-search-bar--submit-button">
-            <input class="button" type="submit" value="<?php echo esc_attr( __( 'Find Listings', 'another-wordpress-classifieds-plugin' ) ); ?>" />
+            <input class="button" type="submit" value="<?php echo esc_attr( __( 'Raaddi', 'another-wordpress-classifieds-plugin' ) ); ?>" />
         </div>
-        <div class="awpcp-classifieds-search-bar--advanced-search-link"><a href="<?php echo esc_url( $action_url ); ?>"><?php echo esc_html( __( 'Advanced Search', 'another-wordpress-classifieds-plugin' ) ); ?> </a></div>
     </form>
 </div>
